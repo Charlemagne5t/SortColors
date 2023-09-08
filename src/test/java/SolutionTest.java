4d8811e6f02ba59e6c1089254629cb65ac1e2f99
alek <wait82985@gmail.com>
2023-09-08 11:57:42 +0600

    initial commit. 2 test cases

---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,22 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void sortColorsTest1() {
+        int[] nums = {2, 0, 2, 1, 1, 0};
+        new Solution().sortColors(nums);
+        int[] expected = {0, 0, 1, 1, 2, 2};
+
+        Assert.assertArrayEquals(expected, nums);
+    }
+
+    @Test
+    public void sortColorsTest2() {
+        int[] nums = {2, 0, 1};
+        new Solution().sortColors(nums);
+        int[] expected = {0, 1, 2};
+
+        Assert.assertArrayEquals(expected, nums);
+    }
+}
